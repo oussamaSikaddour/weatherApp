9f773a5701d9e0ad37f9606385a52db3fb8260ea
oussama sikaddour <34127545+TheOussama@users.noreply.github.com>
2021-03-28 19:20:22 +0200

    added serveless fucntions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -11,12 +11,16 @@ export const getHomeLocation = () => {
   return localStorage.getItem("defaultWeaherLocation");
 };
 export const getWeaherFromCoords = async (locationObj) => {
-  const lat = locationObj.getLat();
-  const lon = locationObj.getLon();
-  const units = locationObj.getUnit();
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerst&unites=${units}&appid=${WEATHER_API_KEY}`;
+  const urlDataObj = {
+    lat: locationObj.getLat(),
+    lon: locationObj.getLon(),
+    units: locationObj.getUnit(),
+  };
   try {
-    const weatherStream = await fetch(url);
+    const weatherStream = await fetch(`./.netlify/functons/get_weather`, {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
     const weatherJson = await weatherStream.json();
     return weatherJson;
   } catch (error) {
@@ -24,17 +28,31 @@ export const getWeaherFromCoords = async (locationObj) => {
   }
 };
 export const getGoordsFromApi = async (entryText, units) => {
-  const regex = /^\d+$/g;
-  const flag = regex.test(entryText) ? "zip" : "q";
-  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
-  const encodedUrl = encodeURI(url);
-  try {
-    const dataStream = await fetch(encodedUrl);
-    const jasonData = await dataStream.json();
+  // const regex = /^\d+$/g;
+  // const flag = regex.test(entryText) ? "zip" : "q";
+  // const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  // const encodedUrl = encodeURI(url);
+  // try {
+  //   const dataStream = await fetch(encodedUrl);
+  //   const jasonData = await dataStream.json();
 
-    return jasonData;
-  } catch {
-    console.log(err.stack);
+  //   return jasonData;
+  // } catch {
+  //   console.log(err.stack);
+  // }
+  const urlDataObj = {
+    text: entryText,
+    units: units,
+  };
+  try {
+    const dataStream = await fetch("./.netlify/functions/get_coords", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
+    const jsonData = await dataStream.json();
+    return jsonData;
+  } catch (err) {
+    console.error(err);
   }
 };
 export const cleanText = (text) => {
